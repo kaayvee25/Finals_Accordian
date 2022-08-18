98a0561da5226feeea0b0503f01688ea354e0b3b
Singh <karanveersinghk@student.douglascollege.ca>
2022-08-18 09:18:26 -0700

    add all the files

---
+++ js_script.js
@@ -0,0 +1,27 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+var acc_left = document.getElementsByClassName("accordion_left");
+var j;
+for (j = 0; j < acc_left.length; j++) {
+    acc_left[j].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+  }
